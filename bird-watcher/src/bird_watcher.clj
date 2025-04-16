afe9d1b0affebabc6c32af37b0dd2814c2263f82
Tiago Corrêa <developer@tiagocorrea.me>
2025-04-16 16:28:17 -0300

    Bird Watcher

---
+++ bird-watcher/src/bird_watcher.clj
@@ -0,0 +1,22 @@
+(ns bird-watcher)
+
+(def last-week 
+  (vector 0 2 5 3 7 8 4))
+
+(defn today [birds]
+  (last birds))
+
+(defn inc-bird [birds]
+  (update birds (dec (count birds)) inc))
+
+(defn day-without-birds? [birds]
+  (not (every? pos? birds)))
+
+(defn n-days-count [birds n]
+  (reduce + (take n birds)))
+
+(defn busy-days [birds]
+  (count (filter #(<= 5 %) birds)))
+
+(defn odd-week? [birds]
+  (= [1 0 1 0 1 0 1] birds))
