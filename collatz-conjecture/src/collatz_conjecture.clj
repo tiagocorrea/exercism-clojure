3be1ae4a1efc2ce3d8b13417f32998e8393d3e48
Tiago Corrêa <developer@tiagocorrea.me>
2025-04-16 18:31:12 -0300

    Solve Collatz Conjecture

--- collatz-conjecture/src/collatz_conjecture.clj
+++ collatz-conjecture/src/collatz_conjecture.clj
@@ -1,8 +1,13 @@
 (ns collatz-conjecture)
 
+(defn run-steps [n steps]
+  (cond
+    (<= n 1) steps
+    (even? n) (recur (/ n 2) (inc steps))
+    (odd? n) (recur (+ 1 (* n 3)) (inc steps))))
+
 (defn collatz
   "Returns the number of steps it takes to reach 1 according
   to the rules of the Collatz Conjecture."
   [num]
-  ;; function body
-)
+  (run-steps num 0))
