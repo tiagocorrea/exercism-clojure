6080476461d3ca4cccf8eb877a9fd9e37400c400
Tiago Corrêa <developer@tiagocorrea.me>
2025-04-17 17:07:12 -0300

    Solve Armstrong Numbers

--- armstrong-numbers/src/armstrong_numbers.clj
+++ armstrong-numbers/src/armstrong_numbers.clj
@@ -3,5 +3,7 @@
 (defn armstrong?
   "Returns true if the given number is an Armstrong number; otherwise, returns false"
   [num]
-  ;; function body
-  )
+  (let [digits (map #(Integer/parseInt (str %)) (str num))
+        len (count digits)]
+    (= num
+       (apply +' (map #(.pow (biginteger %) len) digits)))))
