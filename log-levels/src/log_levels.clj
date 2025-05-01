094bc1ec5cc95af5166a6bcb9a1434c1e2d7186f
Tiago Corrêa <developer@tiagocorrea.me>
2025-05-01 17:50:54 -0300

    Solve Log Levels

--- log-levels/src/log_levels.clj
+++ log-levels/src/log_levels.clj
@@ -4,14 +4,22 @@
 (defn message
   "Takes a string representing a log line
    and returns its message with whitespace trimmed."
-  [s])
+  [s]
+  (-> s
+      (str/split #":")
+      (last)
+      (str/trim)))
 
 (defn log-level
   "Takes a string representing a log line
    and returns its level in lower-case."
-  [s])
+  [s]
+  (->> s
+       (re-find #"(?<=\[)[A-Z]+(?=\])")
+       (str/lower-case)))
 
 (defn reformat
   "Takes a string representing a log line and formats it
    with the message first and the log level in parentheses."
-  [s])
+  [s]
+  (format "%s (%s)" (message s) (log-level s)))
