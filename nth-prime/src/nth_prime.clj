2497b58843b5e85af848c58da796bd37d2bbbd6b
Tiago Corrêa <developer@tiagocorrea.me>
2025-05-01 15:59:54 -0300

    WIP Nth Prime

--- nth-prime/src/nth_prime.clj
+++ nth-prime/src/nth_prime.clj
@@ -1,6 +1,16 @@
 (ns nth-prime)
 
-(defn nth-prime 
+(defn prime?
+  [n]
+  (cond
+    (< n 2) false
+    (= n 2) true
+    :else (->> (+ 1 (Math/sqrt n))
+               (range 2)
+               (filter #(= 0 (mod n %)))
+               (empty?))))
+
+(defn nth-prime
   "Returns the nth prime number."
   [n]
   ;; function body
